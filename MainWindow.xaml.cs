1e161317fa936318595223e5aa953af3fb20640c
Jens Houttekiet <jens.houttekiet@student.vives.be>
2024-03-07 17:42:16 +0100

    GUI, knoppen veranderen, json save file gemaakt zodat de knoppen opslaan dat je insteldOnnodige lijn code verwijderd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,179 +16,225 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using WindowsInput;
 using System.IO.Ports;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace macro_testen
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        bool open;          // Maakt dat de poort niet twee keer open kan.
-        bool sluit = true;         // Maakt dat de poort niet twee keer gesloten kan.
-        bool keuze;         // Maakt dat de niet open kan voordat een com poort gekozen is.
         public string Poort { get; set; }
         private SerialPort serialPort;
-        byte open_dicht = 1;
-        byte aangesloten = 0;
+        bool geenComPoort = true;
+        bool site;
 
         private readonly InputSimulator simulator;
+        private SavedData data;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeSerialPort();
             simulator = new InputSimulator();
+            data = JsonManager.LoadData();
+            LaadData();
+        }
+        private void InitializeSerialPort()
+        {
+            while (geenComPoort == true) // blijft zoeken achter COM-poort indien geen gevonden
+            {
+                // zoek alle mogelijke poorten
+                String[] Ports = SerialPort.GetPortNames();
+                COMpoortenlijst.Items.Clear();
+
+                foreach (string poort in Ports) // zet alle COM-poorten in een lijst om te zien
+                {
+                    COMpoortenlijst.Items.Add(poort);
+                }
+                COMpoortenlijst.SelectedIndex = 0;
+
+                foreach (string poort in Ports) // overloopt alle gevonden COM-poorts
+                {
+                    SerialPort port = new SerialPort(poort);
+                    try // opent poort en zoekt achter data
+                    {
+                        port.Open();
+                        SerialPort data = port;
+                        String indata = data.ReadExisting();
+                        string trimmedData = indata.Trim();
+                        RecivedData.Content = trimmedData;
+                        if (trimmedData == "10") // als er een 10 wordt ontvangen stopt de controleloop (van de microcontroller)
+                        {
+                            port.DataReceived += DatareceivedHandler;
+                            try
+                            {
+                                serialPort = port;
+                                serialPort.Write("0");
+                            }
+                            catch (Exception ex)
+                            {
+                                System.Windows.MessageBox.Show($"Error writing to serial port: {ex.Message}");
+                            }
+                            geenComPoort = false;
+                        }
+                        if (indata.Trim() != "10") // indien er niets wordt ontvangen, sluit de poort opnieuw
+                        {
+                            port.Close();
+                        }
+                    }
+                    catch (Exception ex) // meld probleem bij een poort
+                    {
+                        System.Windows.MessageBox.Show($"Error accessing port {poort}: {ex.Message}");
+                    }
+                }
+            }
         }
         private void keypress_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void CheckPorts_Click(object sender, RoutedEventArgs e)
-        {
-            String[] Ports = SerialPort.GetPortNames();    // checkt welke poorten verbonden zijn.
-            COMpoortenlijst.Items.Clear();                 // Verwijderd de compoorten die al in de lijst staan.
-            foreach (string poort in Ports)                // Elke verbonden poort word in een string geplaatst.
-            {
-                COMpoortenlijst.Items.Add(poort);          // Zet de poorten in de list box.
-            }
-            if (Ports.Length > 0)
-            {
-                COMpoortenlijst.SelectedIndex = 0;         // Maakt zodat er zeker een poort word gekozen.
-                OpenPoort.IsEnabled = true;                // Activeer de openButton als er poorten zijn gevonden
-  
-            }                                              // Toont aan dat er een poort gekozen is.
-            else
-            {
-                OpenPoort.IsEnabled = false;
-                System.Windows.MessageBox.Show("Geen COM-poorten gevonden.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
-        }
-        private void OpenPoort_Click(object sender, RoutedEventArgs e)
-        {
-            if(serialPort != null && serialPort.IsOpen)
-            {
-                System.Windows.MessageBox.Show("COM-poort is al geopend.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                return;
-            }
-
-            string selectedPort = COMpoortenlijst.SelectedItem as string;
-
-            if(selectedPort != null)
-            {
-                serialPort = new SerialPort(selectedPort);
-                serialPort.DataReceived += DatareceivedHandler;
-                try
-                {
-                    serialPort.Open();
-                    System.Windows.MessageBox.Show("COM-poort is geopend.", "Sucess", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                catch (Exception ex)
-                {
-                    System.Windows.MessageBox.Show("Fout bij het openen an COM-poort: " + ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-            else
-            {
-                System.Windows.MessageBox.Show("Selecteer een COM-poort om te openen", "ERROR", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
-        }
-
-        private void SluitPoort_Click(object sender, RoutedEventArgs e)
-        {
-            if (sluit == false)
-            {
-                serialPort.Close();           // Sluit de com poort.
-                aangesloten = 0;
-                open = false;               // Maakt dat de com poort weer open kan.
-                sluit = true;               // Maakt dat de com poort niet opnieuw kan gesloten worden.
-            }
-            else
-            {
-                System.Windows.MessageBox.Show("COM poort is al gesloten.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Exclamation); // Maakt dat gebruiker weet dat com poort al gesloten is.
-            }
-        }
         private void DatareceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort data = (SerialPort)sender;
             String indata = data.ReadExisting();
-            // System.Windows.MessageBox.Show(indata, "DATA", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            switch (Convert.ToInt32(indata))
+            string trimmedData = indata.Trim();
+            System.Threading.Thread.Sleep(200);
+            if (trimmedData != "10")
             {
-                case 1:
-                    Button1();
-                    break;
-                case 2:
-                    Button2();
-                    break;
-                case 3:
-                    Button3();
-                    break;
-                case 4:
-                    Button4();
-                    break;
-                case 5:
-                    Button5();
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
+                SimulateWinRKeyPress();
+                if (site == true)
+                {
+                    System.Windows.Controls.TextBox textBox = sender as System.Windows.Controls.TextBox;
+                    int textBoxIndex = int.Parse(textBox.Name.Substring(4));
+                    string siteName = "site" + textBoxIndex;
+                    UIElement TBtekst = FindName(siteName) as UIElement;
+                    SendKeys.SendWait(((System.Windows.Controls.TextBox)TBtekst).Text.ToString());
+                }
+                else
+                {
+                    //SendKeys.SendWait(data.VarPrograms[int.Parse(indata)]);
+                    System.Windows.MessageBox.Show("yur");
+                }
+                simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
             }
         }
-
         private void SimulateWinRKeyPress()
         {
             simulator.Keyboard.ModifiedKeyStroke(WindowsInput.Native.VirtualKeyCode.LWIN, WindowsInput.Native.VirtualKeyCode.VK_R);
         }
+        public class SavedData
+        {
+            public string[] Sites { get; set; } = new string[9];
+            public string[] Programs { get; set; } = new string[9];
+            public string[] VarPrograms { get; set; } = new string[9];
+            public bool[] site { get; set; } = new bool[9]; // site of program
+        }
+        public class JsonManager
+        {
+            private const string FilePath = "SavedData.json";
 
-        private void Button1()
-        {
-            SimulateWinRKeyPress();
-            System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("https://www.facebook.com/");
-            simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
-        }
+            public static SavedData LoadData()
+            {
+                if (File.Exists(FilePath))
+                {
+                    string json = File.ReadAllText(FilePath);
+                    return JsonConvert.DeserializeObject<SavedData>(json);
+                }
+                return new SavedData();
+            }
 
-        private void Button2()
-        {
-            SimulateWinRKeyPress();
-            System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("Notepad");
-            simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
+            public static void SaveData(SavedData data)
+            {
+                string json = JsonConvert.SerializeObject(data);
+                File.WriteAllText(FilePath, json);
+            }
         }
+        private void LaadData()
+        {
+            string[] items = { "Excel", "Google", "Notepad", "Outlook", "Site", "Word" };
+            System.Windows.Controls.ComboBox[] comboBoxes = { CB1, CB2, CB3, CB4, CB5, CB6, CB7, CB8, CB9};
 
-        private void Button3()
-        {
-            System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("Hello, World!");
-        }
-                
-        private void Button4()
-        {
-            SimulateWinRKeyPress();
-            System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("winword");
-            simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
-        }
+            foreach (System.Windows.Controls.ComboBox cb in comboBoxes)
+            {
+                foreach (string item in items)
+                {
+                    cb.Items.Add(item);
+                }
+            }
+            CB1.SelectedItem = data.Programs[1];
+            site1.Text = data.Sites[1];
 
-        private void Button5()
+            for (int i = 1; i <= 9; i++)
+            {
+                string siteName = "site" + i;
+                UIElement TBtekst = FindName(siteName) as UIElement;
+                string CBName = "CB" + i;
+                UIElement CBElement = FindName(CBName) as UIElement;
+                ((System.Windows.Controls.ComboBox)CBElement).SelectedItem = data.Programs[i];
+                ((System.Windows.Controls.TextBox)TBtekst).Text = data.Sites[i];
+            }
+        }
+        private void Debug_Click(object sender, RoutedEventArgs e)
         {
             SimulateWinRKeyPress();
             System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("Excel");
+            if (site == true)
+            {
+                SendKeys.SendWait(site1.Text.ToString());
+            }
+            else
+            {   
+                SendKeys.SendWait(data.VarPrograms[1]);
+            }
             simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
         }
-        
-        private void Outlook_Click(object sender, RoutedEventArgs e)
+        private void CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            SimulateWinRKeyPress();
-            System.Threading.Thread.Sleep(200);
-            SendKeys.SendWait("Outlook");
-            simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.EXECUTE);
+            System.Windows.Controls.ComboBox comboBox = sender as System.Windows.Controls.ComboBox;
+            int comboBoxIndex = int.Parse(comboBox.Name.Substring(2));
+            string selectedItem = comboBox.SelectedItem.ToString();
+            data.Programs[comboBoxIndex] = selectedItem;
+            string siteName = "site" + comboBoxIndex;
+            UIElement siteElement = FindName(siteName) as UIElement;
+            for (int i = 1; i <= 9; i++)
+            {
+                data.site[i] = false;
+            }
+            siteElement.Visibility = Visibility.Hidden;
+
+            switch (selectedItem)
+            {
+                case "Site":
+                    data.site[comboBoxIndex] = true;
+                    siteElement.Visibility = Visibility.Visible;
+                    break;
+                case "Notepad":
+                    data.VarPrograms[comboBoxIndex] = "notepad";
+                    break;  
+                case "Word":
+                    data.VarPrograms[comboBoxIndex] = "winword"; 
+                    break;
+                case "Excel":
+                    data.VarPrograms[comboBoxIndex] = "excel";
+                    break;
+                case "Outlook":
+                    data.VarPrograms[comboBoxIndex] = "outlook";
+                    break;
+                case "Google":
+                    data.VarPrograms[comboBoxIndex] = "https://www.google.com/";
+                    break;
+            }
+
+            JsonManager.SaveData(data);
+        }
+        private void site_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            System.Windows.Controls.TextBox textBox = sender as System.Windows.Controls.TextBox;
+            int textBoxIndex = int.Parse(textBox.Name.Substring(4));
+            string siteName = "site" + textBoxIndex;
+            UIElement TBtekst = FindName(siteName) as UIElement;
+            data.Sites[textBoxIndex] = ((System.Windows.Controls.TextBox)TBtekst).Text.ToString();
+            JsonManager.SaveData(data);
         }
     }
 }
